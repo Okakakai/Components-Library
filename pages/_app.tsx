c81f5050044274504e311676bd12ef6d888c37d2
Okakakai <inoue.kai.work@gmail.com>
2023-06-06 15:56:39 +0900

    add reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+          li {
+            list-style: none;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
