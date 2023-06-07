d31207168950fb98bf528346f498689c0d8b2bd8
Okakakai <inoue.kai.work@gmail.com>
2023-06-07 23:29:42 +0900

    add Border Margin

--- pages/components/layouts/SplitLayout.tsx
+++ pages/components/layouts/SplitLayout.tsx
@@ -2,11 +2,13 @@ import styled from '@emotion/styled';
 import { ChakraBreakPoint } from '../../../utils/mediaQueryUtils';
 import Split from '../../../layout/split';
 import { ThreeColumnLayout } from '../../../components/ThreeColumnLayout';
+import { StyledBorderBox } from '../../../components/border/BorderBox';
 
 const SplitLayout = () => {
   return (
     <Container>
       <Split />
+      <StyledBorderBox />
       <ThreeColumnLayout />
       <Split />
     </Container>
