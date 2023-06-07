6ee6c22b9a1dca6466e8b389d362cb457a1dc200
Okakakai <inoue.kai.work@gmail.com>
2023-06-07 23:27:59 +0900

    add sample

--- pages/components/layouts/SplitLayout.tsx
+++ pages/components/layouts/SplitLayout.tsx
@@ -1,21 +1,22 @@
 import styled from '@emotion/styled';
 import { ChakraBreakPoint } from '../../../utils/mediaQueryUtils';
 import Split from '../../../layout/split';
+import { ThreeColumnLayout } from '../../../components/ThreeColumnLayout';
 
 const SplitLayout = () => {
   return (
     <Container>
+      <Split />
+      <ThreeColumnLayout />
       <Split />
     </Container>
   );
 };
 
 const Container = styled.div`
+  margin-top: 5rem;
   width: 100%;
-  height: 200vh;
-  ${ChakraBreakPoint.mq.lg} {
-    height: 50vh;
-  }
+  height: 100%;
 `;
 
 export default SplitLayout;
